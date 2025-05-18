538940e1a2d34f63586a32390a5bf604d8ab9af7
Sevmamra <sevmamra01@gmail.com>
2025-05-18 09:07:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,44 +1,22 @@
-"""
-from os import getenv
-
-
-API_ID = int(getenv("API_ID", "26797881"))
-API_HASH = getenv("API_HASH", "9699262c708c2e45ba18bfce925ed5ed")
-BOT_TOKEN = getenv("BOT_TOKEN", "7734880223:AAHG5PexgS7JdRTipt51udOg6uoINAsEjuM")
-OWNER_ID = int(getenv("OWNER_ID", "6567162029"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050277919 2112898623").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://sevmamra01:<db_password>@cluster0.ff29e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002034072106"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002303981738"))
-
-"""
-#
-
-
-
-
-# --------------M----------------------------------
+# config.py - Secure Configuration File
+# NEVER SHARE THIS FILE WITH ACTUAL CREDENTIALS
 
 import os
 from os import getenv
-# ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID"))
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH")
-# ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-# -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
-# ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-# ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5840594311 7621154046 7793979196 5798579221").split()))
-# ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
-# ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL")
-# -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS"))
+# ===== TELEGRAM CONFIGURATION =====
+API_ID = int(getenv("API_ID", "26797881"))  # my.telegram.org से लें
+API_HASH = getenv("API_HASH", "9699262c708c2e45ba18bfce925ed5ed")  # my.telegram.org से लें
+BOT_TOKEN = getenv("BOT_TOKEN", "7734880223:AAHG5PexgS7JdRTipt51udOg6uoINAsEjuM")  # @BotFather से लें
+BOT_USERNAME = getenv("BOT_USERNAME", "apnatxtwaalabot")  # बिना @ के
 
+# ===== USER PERMISSIONS =====
+OWNER_ID = int(getenv("OWNER_ID", "6567162029"))  # आपका Telegram User ID
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6567162029").split()))
+
+# ===== DATABASE CONFIG =====
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://sevmamra01:<db_password>@cluster0.ff29e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB Atlas से
+
+# ===== CHANNEL CONFIG =====
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002303981738"))  # -100 के साथ
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002303981738"))  # -100 के साथ
